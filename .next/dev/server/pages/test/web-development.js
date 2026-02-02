628d281d1fe4202bb69469b31571ca8eec006be3
Mounish P <codevivestudios@gmail.com>
2026-02-02 13:05:53 +0530

    removed .next

--- .next/dev/server/pages/test/web-development.js
+++
@@ -1,8 +0,0 @@
-var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/pages/test/web-development.js")
-R.c("server/chunks/ssr/node_modules_next_526e1131._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__09737d6e._.js")
-R.c("server/chunks/ssr/node_modules_186c80ea._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__e6a4d965._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__b1dd1a27._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/test/web-development.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/test/web-development.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
