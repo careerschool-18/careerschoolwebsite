c9d6691dbb9cf602d679c307fee220937ce630f9
mouliprasanth-123 <mouliprasanth2003@gmail.com>
2026-03-18 17:35:42 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,5 @@
 import "../styles/globals.css";
-import Script from "next/script";
-import "react-phone-input-2/lib/style.css";
-import { Toaster } from "react-hot-toast";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      {/* Google Analytics */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-H2Y6DYC4SX"
-        strategy="afterInteractive"
-      />
-
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-H2Y6DYC4SX');
-        `}
-      </Script>
-
-      {/* ✅ Toast */}
-      <Toaster position="top-right" />
-
-      <Component {...pageProps} />
-    </>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
