43c496a59e489ab32ab525e8c6b3d6ceadc6dadf
mouliprasanth-123 <mouliprasanth2003@gmail.com>
2026-03-18 17:36:14 +0530

    Enhance document head with meta tags and analytics
    Added meta tags for language, robots, description, and keywords. Included Google Analytics scripts for tracking.

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,35 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Global tags only */}
-
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="language" content="English" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="description"
+          content="India’s #1 Training & Placement Institute for Python, Full Stack Development, Java, Web Development, Digital Marketing, Data Analytics, HR and more. Learn, Intern & Get Placed with Careerschool!"
+        />
+        <meta
+          name="keywords"
+          content="careerschool, python training, full stack training, data analytics training, digital marketing training"
+        />
+        <link rel="icon" href="/Fav icon/Fav Icon.png" />
 
+        {/* ✅ Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-H2Y6DYC4SX"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-H2Y6DYC4SX');
+            `,
+          }}
+        />
       </Head>
-
       <body>
         <Main />
         <NextScript />
