ccc4fc411b51dbb662e9fdfc59ef5c1502acb7a2
Careerschool - Sarathi <217547806+careerschool18@users.noreply.github.com>
2026-06-11 17:00:09 +0530

    url

--- pages/HRLogin.js
+++ pages/HRLogin.js
@@ -10,7 +10,7 @@ export default function LoginPage() {
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://72.60.220.80:8080/api/auth/login", {
+      const response = await fetch("https://72.60.220.80:8080/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
