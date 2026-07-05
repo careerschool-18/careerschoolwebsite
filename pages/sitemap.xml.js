7da67ef93b1b507c7e53259b9843b6700dc42373
AghamarshK <aghamarshkuppachi@gmail.com>
2026-07-05 19:48:33 +0530

    Sitemap URL fix for nellore python

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -6,7 +6,7 @@ export async function getServerSideProps({ res }) {
     "/data-analytics-ai-course",
     "/online-assessment",
     "/training-enquiry-form",
-    "/nellorepython",
+    "/nellore-python",
     "/careerschool-jobs"
   ];
 
